2f48e6cb62980784ca3406c92cf244af88528b9c
saheryadav <82718832+saheryadav@users.noreply.github.com>
2021-08-21 21:39:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+ctx.beginPath();
+ctx.strokeStyle="grey";
+ctx.lineWidth= 4;
+ctx.rect(150,143,430,200)
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth= 5;
+ctx.arc(250, 210, 40, 0, 2 *Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth= 5;
+ctx.arc(350, 210, 40, 0, 2 *Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth= 5;
+ctx.arc(450, 210, 40, 0, 2 *Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth= 5;
+ctx.arc(550, 210, 40, 0, 2 *Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth= 5;
+ctx.arc(650, 210, 40, 0, 2 *Math.PI);
+ctx.strokeStyle();
